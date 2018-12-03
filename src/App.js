761610b97580f7eaac5c03f53f791bae58b2b256
syed <shah_h6@yahoo.com>
2018-12-03 14:16:33 +0900

    updates in servers url

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
     onButtonSubmit=()=>{
       this.setState({imageUrl:this.state.input});
 
-      fetch('http://localhost:3001/imageurl',{
+      fetch('https://dry-tundra-29533.herokuapp.com/imageurl',{
             method:'post',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({
@@ -107,7 +107,7 @@ class App extends Component {
       // this.state.input)
       .then(response=>{
         if(response){
-          fetch('http://localhost:3001/image',{
+          fetch('https://dry-tundra-29533.herokuapp.com/image',{
             method:'put',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -21,7 +21,7 @@ class SignIn extends React.Component{
 	}
 
 	onSubmitSignIn=()=>{
-		fetch('http://localhost:3001/signin',{
+		fetch('https://dry-tundra-29533.herokuapp.com/signin',{
 			method:'post',
 			headers:{'Content-Type':'application/json'},
 			body:JSON.stringify({
